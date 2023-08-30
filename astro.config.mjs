d0dd47a676121d0c5815859d7593d3be376440e5
Ehren12 <nwokochaehren@gmail.com>
2023-08-30 22:05:10 +0100

    Add pwa support 📱

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import AstroPWA from '@vite-pwa/astro'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    AstroPWA({
+      /* your pwa options */
+    })
+  ]
+})
